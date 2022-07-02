2e430c0b37fab5e147e7b289f73d9b8dfd7fa0fe
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-02 05:00:04 +0300

    добавила решение второй задачи

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите два числа ");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+if (a > b)
+{
+Console.Write("max = ");
+Console.WriteLine(a);
+Console.Write("min = ");
+Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+Console.WriteLine(a);
+}
