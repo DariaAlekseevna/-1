44171c6e5b67b8c325e2e666fc85793110d8f7dd
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-02 06:13:36 +0300

    добавила решение восьмой задачи немного поправила шестую задачу

--- task 6/Program.cs
+++ task 6/Program.cs
@@ -5,11 +5,13 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number % 2 == 0)
 {
+    Console.Write("Да, ");
      Console.Write(number);
     Console.Write(" чётное");
 } 
 else 
 {
+    Console.Write("Нет, ");
      Console.Write(number);
    Console.Write(" нечётное"); 
 }
---
+++ task 8/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) 
+{
+    Console.Write("Введено некорректное число, введите целое положительное число");
+}
+
+int count = 0;
+int N = number;
+
+if (number == 1)
+{
+    Console.Write("нет чётных чисел");
+}
+else while (count < N)
+{
+    if (number % 2 == 0)
+{
+     Console.Write(number);
+     Console.Write(" ");
+     number--;
+     count++;
+} 
+else 
+{
+    number--;
+    count++;
+}
+
+ }
+
